875b96797dcce384bf4c8e558a5c75b65f14a0ac
maxmsi <darkmeshas@gmail.com>
2023-03-14 12:08:19 +0100

    Exception handling globally with @ControllerAdvice.

--- src/main/java/com/babikk/dbDemo/rest/StudentRestController.java
+++ src/main/java/com/babikk/dbDemo/rest/StudentRestController.java
@@ -35,17 +35,6 @@ public class StudentRestController {
        return students.get(studentId);
     }
 
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleExc(StudentNotFoundException exc){
-         StudentErrorResponse error =new StudentErrorResponse();
-
-         error.setStatus(HttpStatus.NOT_FOUND.value());
-         error.setMessage(exc.getMessage());
-         error.setTimeStamp(System.currentTimeMillis());
-
-         return new ResponseEntity(error, HttpStatus.NOT_FOUND);
-    }
-
 
 
 }
