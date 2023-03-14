0751566751b97917c31afa5dee1c0b793a8b72b2
maxmsi <darkmeshas@gmail.com>
2023-03-14 11:37:46 +0100

    RESTPOINT with Path Variable.

--- src/main/java/com/babikk/dbDemo/rest/StudentRestController.java
+++ src/main/java/com/babikk/dbDemo/rest/StudentRestController.java
@@ -2,8 +2,10 @@ package com.babikk.dbDemo.rest;
 
 import com.babikk.dbDemo.dao.StudentDAO;
 import com.babikk.dbDemo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +14,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+
+    List<Student> students;
     @Autowired
     StudentDAO dao;
+
+    @PostConstruct
+    void loadData(){
+       this.students  = dao.findAll();
+    }
 @GetMapping("/students")
     public List<Student> getStudents(){
-    List<Student> result  = dao.findAll();
-    return result;
+    return students;
 }
 
+    @GetMapping("/students/{studentId}")
+    public Student getStudentsById(@PathVariable int studentId){
+       return students.get(studentId);
+    }
 }
 
